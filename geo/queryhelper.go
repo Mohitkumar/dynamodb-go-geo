ca2050b2df4ef15565381b89aa431dd12cd14c9e
mohitkumar <ch.mohitkumar89@gmail.com>
2018-12-10 09:59:47 +0530

    partial queryhelper implementation

--- geo/queryhelper.go
+++ geo/queryhelper.go
@@ -4,6 +4,29 @@ import (
 	"github.com/golang/geo/s2"
 )
 
-func GeoHashRanges(rect s2.Rect) []HashRange {
+//HashRanges get all ranges
+func HashRanges(rect s2.Rect) []HashRange {
+	cellids := FindCellIds(rect)
+	return mergCells(cellids)
+}
 
+func mergCells(cellUniun s2.CellUnion) []HashRange {
+	cellIds := []s2.CellID(cellUniun)
+	ranges := make([]HashRange, len(cellIds))
+	for _, cellID := range cellIds {
+		hashRange := NewHashRange(uint64(cellID.RangeMin()), uint64(cellID.RangeMax()))
+		wasMerged := false
+
+		for _, r := range ranges {
+			merged := r.merge(hashRange)
+			if merged {
+				break
+				wasMerged = true
+			}
+		}
+		if !wasMerged {
+			ranges = append(ranges, hashRange)
+		}
+	}
+	return ranges
 }
